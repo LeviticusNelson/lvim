a0f6429c6808bff23a75b868e89b98ce2370e17a
Levi Nelson <levibb11@gmail.com>
2022-05-22 13:29:37 -0700

    formatting

--- config.lua
+++ config.lua
@@ -65,18 +65,18 @@ lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-        "bash",
-        "c",
-        "javascript",
-        "json",
-        "lua",
-        "python",
-        "typescript",
-        "tsx",
-        "css",
-        "rust",
-        "java",
-        "yaml",
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -112,17 +112,17 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-        -- { command = "black", filetypes = { "python" } },
-        -- { command = "isort", filetypes = { "python" } },
-        {
-                -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-                command = "prettier",
-                ---@usage arguments to pass to the formatter
-                -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-                extra_args = { "--print-with", "100" },
-                ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-                filetypes = { "typescript", "typescriptreact" },
-        },
+  -- { command = "black", filetypes = { "python" } },
+  -- { command = "isort", filetypes = { "python" } },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    extra_args = { "--print-with", "100" },
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact" },
+  },
 }
 
 -- -- -- set additional linters
@@ -145,40 +145,40 @@ formatters.setup {
 
 -- Additional Plugins
 lvim.plugins = {
-        { "folke/tokyonight.nvim" },
-        {
-                "folke/trouble.nvim",
-                cmd = "TroubleToggle",
-        },
-        {
-                "windwp/nvim-ts-autotag",
-                event = "InsertEnter",
-                config = function()
-                        require("nvim-ts-autotag").setup()
-                end,
-        },
-        {
-                "sindrets/diffview.nvim",
-                event = "BufRead",
-        },
-        {
-                "p00f/nvim-ts-rainbow",
-        },
-        {
-                "folke/lsp-colors.nvim",
-                event = "BufRead",
-        },
-        {
-                "rktjmp/lush.nvim",
-        },
+  { "folke/tokyonight.nvim" },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+  {
+    "p00f/nvim-ts-rainbow",
+  },
+  {
+    "folke/lsp-colors.nvim",
+    event = "BufRead",
+  },
+  {
+    "rktjmp/lush.nvim",
+  },
 }
 
 lvim.builtin.treesitter.rainbow.enable = true
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
-        { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
-        { "BufWinEnter", "*.cls,*.trigger", "set filetype=apexcode" }
+  { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+  { "BufWinEnter", "*.cls,*.trigger", "set filetype=apexcode" }
 }
 
 local lspconfig = require "lspconfig"
@@ -196,44 +196,44 @@ local apex_lsp_name = "apex-lsp"
 
 -- Add Apex to LSP Configs
 configs[apex_lsp_name] = {
-        default_config = {
-                filetypes = { "apexcode" },
-                root_dir = lspconfig.util.root_pattern "sfdx-project.json",
-        },
+  default_config = {
+    filetypes = { "apexcode" },
+    root_dir = lspconfig.util.root_pattern "sfdx-project.json",
+  },
 }
 
 local root_dir = server.get_server_root_path(apex_lsp_name)
 
 local apex_lsp_server = server.Server:new {
-        name = apex_lsp_name,
-        root_dir = root_dir,
-        homepage = "https://developer.salesforce.com/tools/vscode/en/apex/language-server",
-        languages = { "apexcode" },
-        installer = function(ctx)
-                ctx.fs:mkdir "salesforcedx-vscode-git"
-                ctx:chdir("salesforcedx-vscode-git", function()
-                        git.clone({ "https://github.com/forcedotcom/salesforcedx-vscode", recursive = true })
-                end)
-                ctx.fs:rename(
-                        path.concat {
-                                "salesforcedx-vscode-git",
-                                "packages",
-                                "salesforcedx-vscode-apex",
-                                "out",
-                        },
-                        "out"
-                )
-                ctx.fs:rmrf "salesforcedx-vscode-git"
-        end,
-        default_options = {
-                cmd = {
-                        "java",
-                        "-cp",
-                        path.concat { root_dir, "out/apex-jorje-lsp.jar" },
-                        "-Ddebug.internal.errors=true",
-                        "apex.jorje.lsp.ApexLanguageServerLauncher",
-                },
-        },
+  name = apex_lsp_name,
+  root_dir = root_dir,
+  homepage = "https://developer.salesforce.com/tools/vscode/en/apex/language-server",
+  languages = { "apexcode" },
+  installer = function(ctx)
+    ctx.fs:mkdir "salesforcedx-vscode-git"
+    ctx:chdir("salesforcedx-vscode-git", function()
+      git.clone({ "https://github.com/forcedotcom/salesforcedx-vscode", recursive = true })
+    end)
+    ctx.fs:rename(
+      path.concat {
+        "salesforcedx-vscode-git",
+        "packages",
+        "salesforcedx-vscode-apex",
+        "out",
+      },
+      "out"
+    )
+    ctx.fs:rmrf "salesforcedx-vscode-git"
+  end,
+  default_options = {
+    cmd = {
+      "java",
+      "-cp",
+      path.concat { root_dir, "out/apex-jorje-lsp.jar" },
+      "-Ddebug.internal.errors=true",
+      "apex.jorje.lsp.ApexLanguageServerLauncher",
+    },
+  },
 }
 
 servers.register(apex_lsp_server)
@@ -242,13 +242,20 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 -- Call setup
 for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup {
-                capabilities = capabilities,
-                flags = {
-                        -- default in neovim 0.7+
-                        debounce_text_changes = 150,
-                }
-        }
+  lspconfig[lsp].setup {
+    capabilities = capabilities,
+    flags = {
+      -- default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
 end
 
 require("lvim.lsp.manager").setup("apex-lsp")
+vim.cmd([[
+function Packing()
+        :PackerInstall
+        :q
+        :q
+end
+]])
