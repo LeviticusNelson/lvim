a90ec8af3b1e1bb2d614ade7c6ae5485303d33d6
Levi Nelson <levibb11@gmail.com>
2022-06-04 18:12:46 -0700

    removed minimap

--- config.lua
+++ config.lua
@@ -204,16 +204,6 @@ lvim.plugins = {
                 end,
         },
         {
-                'wfxr/minimap.vim',
-                run = "cargo install --locked code-minimap",
-                -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-                config = function()
-                        vim.cmd("let g:minimap_width = 10")
-                        vim.cmd("let g:minimap_auto_start = 1")
-                        vim.cmd("let g:minimap_auto_start_win_enter = 1")
-                end,
-        },
-        {
                 "kevinhwang91/nvim-bqf",
                 event = { "BufRead", "BufNew" },
                 config = function()
